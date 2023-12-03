3db7676de28972a0602eff6a69be6a5d30ed2220
Frank <147031176+frank-suwen@users.noreply.github.com>
2023-12-03 03:03:17 +0000

    fix layout issue

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -5,20 +5,22 @@ const AuthPage = () => {
   return (
     <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
         <Container maxW={"container.md"} padding={0}>
-            {/* left hand side */}
-            <Box display={{ base: "none", md: "block"}}>
-                <Image src="/auth.png" h={650} alt="Phone IMG" />
-            </Box>
+            <Flex justifyContent={"center"} alignItems={"center"} gap={10}>
+                {/* left hand side */}
+                <Box display={{ base: "none", md: "block"}}>
+                    <Image src="/auth.png" h={650} alt="Phone IMG" />
+                </Box>
 
-            {/* right hand side */}
-            <VStack spacing={4} align={"stretch"}>
-                <AuthForm />
-                <Box textAlign={"center"}>Get the app.</Box>
-                <Flex gap={5} justifyContent={"center"}>
-                    <Image src="/playstore.png" h={"10"} alt="Playstore LOGO" />
-                    <Image src="/microsoft.png" h={"10"} alt="Microsoft LOGO" />
-                </Flex>
-            </VStack>
+                {/* right hand side */}
+                <VStack spacing={4} align={"stretch"}>
+                    <AuthForm />
+                    <Box textAlign={"center"}>Get the app.</Box>
+                    <Flex gap={5} justifyContent={"center"}>
+                        <Image src="/playstore.png" h={"10"} alt="Playstore LOGO" />
+                        <Image src="/microsoft.png" h={"10"} alt="Microsoft LOGO" />
+                    </Flex>
+                </VStack>
+            </Flex>
         </Container>
     </Flex>
   )
