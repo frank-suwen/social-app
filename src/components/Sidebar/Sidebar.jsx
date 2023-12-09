c2138129b8313b265512b3c76ae915a21de9de78
Frank <147031176+frank-suwen@users.noreply.github.com>
2023-12-09 06:14:59 +0000

    add logout

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import { Avatar, Box, Flex, Link, Tooltip } from "@chakra-ui/react"
 import { Link as RouterLink } from "react-router-dom"
 import { InstagramLogo, InstagramMobileLogo, CreatePostLogo, NotificationsLogo, SearchLogo } from "../../assets/constants"
 import { AiFillHome } from "react-icons/ai"
+import { BiLogOut } from "react-icons/bi"
 
 const Sidebar = () => {
   const sidebarItems = [
@@ -81,6 +82,7 @@ const Sidebar = () => {
                 borderRadius={6}
                 p={2}
                 w={{ base: 10, md: "full"}}
+                justifyContent={{ base: "center", md: "flex-start" }}
               >
                 {item.icon}
                 <Box display={{ base: "none", md: "block" }}>{item.text}</Box>
@@ -88,6 +90,33 @@ const Sidebar = () => {
             </Tooltip>   
           ))}
         </Flex>
+
+        <Tooltip
+              hasArrow
+              label={"Logout"}
+              placement="right"
+              ml={1}
+              openDelay={500}
+              display={{ base: "block", md: "none"}}
+            >
+              <Link
+                display={"flex"}
+                to={"/auth"}
+                as={RouterLink}
+                alignItems={"center"}
+                gap={4}
+                _hover={{ bg: "whiteAlpha.400" }}
+                borderRadius={6}
+                p={2}
+                w={{ base: 10, md: "full"}}
+                mt={"auto"}
+                justifyContent={{ base: "center", md: "flex-start" }}
+              >
+                <BiLogOut size={25} />
+                <Box display={{ base: "none", md: "block" }}>Logout</Box>
+              </Link>
+            </Tooltip>
+
       </Flex>
 
     </Box>
