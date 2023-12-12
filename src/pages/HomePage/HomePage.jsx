fba16dc1c5360d25f4a6b766618d6975860c3ab9
Frank <147031176+frank-suwen@users.noreply.github.com>
2023-12-12 04:50:54 +0000

    addthe basic structure for HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,19 @@
+import { Container, Flex, Box } from "@chakra-ui/react"
+
 
 const HomePage = () => {
     return (
-        <div>HomePage</div>
+        <Container maxW={"container.lg"}>
+            <Flex gap={20}>
+                <Box flex={2} py={10} border={"1px solid blue"}>
+                    FeedPosts
+                </Box>
+
+                <Box flex={3} mr={20} display={{ base: "none", lg: "block" }} maxW={"300px"} border={"1px solid red"}>
+                    Suggested Users
+                </Box>
+            </Flex>
+        </Container>
     ) 
 }
 
