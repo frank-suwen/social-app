bb8bd59ac6a9dac68e53d7d3686cbe62ec784ec5
Frank <147031176+frank-suwen@users.noreply.github.com>
2023-12-12 23:39:26 +0000

    add comment section

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Text } from "@chakra-ui/react"
+import { Flex, Box, Text, InputGroup, Input, InputRightAddon, InputRightElement, Button } from "@chakra-ui/react"
 import { useState } from "react"
 import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constants"
 
@@ -17,7 +17,7 @@ const PostFooter = () => {
   }
 
   return (
-    <>
+    <Box mb={10}>
       <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
         <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
           {!liked ? <NotificationsLogo /> : <UnlikeLogo />}
@@ -37,7 +37,25 @@ const PostFooter = () => {
       <Text fontSize="sm" color={"gray"}>
         View all 1,000 comments
       </Text>
-    </>
+
+      <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
+        <InputGroup>
+          <Input variant={"flushed"} placeholder={"Add a comment ..."} fontSize={14} />
+          <InputRightElement>
+            <Button
+              fontSize={14}
+              color={"blue.500"}
+              fontWeight={600}
+              cursor={"pointer"}
+              _hover={{ color: "white" }}
+              bg={"transparent"}
+            >
+              Post
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
+    </Box>
   )
 }
 
