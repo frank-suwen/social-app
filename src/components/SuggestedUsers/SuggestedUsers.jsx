86169168585f6ee5df8bc521768987606b3a7d9a
Frank <147031176+frank-suwen@users.noreply.github.com>
2023-12-17 05:45:08 +0000

    add basic layout for SuggestedUsers.jsx

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,11 +1,35 @@
-import { VStack } from "@chakra-ui/react"
+import { VStack, Flex, Text, Box, Link } from "@chakra-ui/react"
 import SuggestedHeader from "./SuggestedHeader"
+import SuggestedUser from "./SuggestedUser"
+
 
 
 const SuggestedUsers = () => {
   return (
     <VStack py={8} px={6} gap={4}>
         <SuggestedHeader />
+
+        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+            <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+                Suggested for you
+            </Text>
+            <Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+                See All
+            </Text>
+        </Flex>
+
+        <SuggestedUser />
+        <SuggestedUser />
+        <SuggestedUser />
+        
+        <Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+            © 2023 Built By {" Frank "}
+            <Link href="https://github.com/frank-suwen" target="_blank" color={"blue.500"} fontSize={14}>
+                As a Programmer
+            </Link>
+        </Box>
+        
+
     </VStack>
   )
 }
