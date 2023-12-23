abf90ce6edab5936b5a8fc5d52629430a73d2968
Frank <147031176+frank-suwen@users.noreply.github.com>
2023-12-23 22:49:16 +0000

    add skeleton on ProfilePosts.jsx

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,8 +1,32 @@
-
+import { Grid, Skeleton, VStack, Box } from "@chakra-ui/react"
+import { useState, useEffect } from "react"
 
 const ProfilePosts = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  }, [])
+
   return (
-    <div>ProfilePosts</div>
+    <Grid
+      templateColumns={{
+        sm: "repeat(1, 1fr)",
+        md: "repeat(3, 1fr)"
+      }}
+      gap={1}
+      columnGap={1}
+    >
+      {isLoading && [0, 1, 2, 3, 4, 5].map((_, idx) => (
+        <VStack key={idx} alignItems={"flex-start"} gap={4}>
+          <Skeleton w={"full"}>
+            <Box h="300px">contents wrapped</Box>
+          </Skeleton>
+        </VStack>
+      ))}
+    </Grid>
   )
 }
 
